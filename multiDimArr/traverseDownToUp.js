80afbf675786af8eb17df7ec30c250cbeaa3134e
Siddharth Bisht <80667996+OfficialSiddharthBisht@users.noreply.github.com>
2021-11-04 12:33:01 +0530

    solved traverse down to up problem

---
+++ multiDimArr/traverseDownToUp.js
@@ -0,0 +1,32 @@
+/*You are given a 2D array(matrix), of N rows and M columns.
+-------------
+Input
+4 3
+1 8 9
+2 7 10
+3 6 11
+4 5 12
+-------------
+Output
+4 3 2 1 5 6 7 8 12 11 10 9
+note - the image of the question is - 
+*/
+function traverseDownToUp(N, M, matrix){
+    //write code here
+    let str="";
+    for(let j=0;j<M;j++)
+    {
+        for(let i=N-1;i>=0;i--)
+        {
+            str+=matrix[i][j]+" ";
+        }
+    }
+  console.log(str);
+}
+let arr=[
+    [1,8,9],
+    [2,7,10],
+    [3,6,11],
+    [4,5,12]
+]
+traverseDownToUp(4,3,arr);

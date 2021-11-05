bd27310811d020d299abd1ba47bf270298b9a979
Siddharth Bisht <80667996+OfficialSiddharthBisht@users.noreply.github.com>
2021-11-05 15:20:20 +0530

    solved check palindrome question by two pointer

--- Arrays And Strings/Arrays And Strings/checkVowel.js
+++ Arrays And Strings/Arrays And Strings/checkVowel.js
@@ -1,25 +1,56 @@
-/* given a string, whose size is stored in a variable with the name N
-The string is stored in a variable with the namestr
-You have to printtrue, if the string contains at least one vowel, otherwise printfalse
-For example, consider the value stored inN = 6, and the value stored instr = "nrupul", then the output will betrue, since the string contains the vowelutwice
-Note : The string only contains lowercase English alphabets
-Note : The vowels in the English Alphabet are considered the following - a,e,i,o,u
+/* Given a string str and its length N , print true if the string constains atleast one vowel else print false
 */
-function checkVowel(N, str) {
-    //write code here
-    let vowels="aeiou";
-    for(let i=0;i<vowels.length;i++)
-    {
-        for(let j=0;j<N;j++)
-        {
-            if(str[j]===vowels[i])
-            {
-                console.log("true");
-                return;
-            }
-        }
-    }
-    console.log("false");
+function runProgram(input) {
+    // Write code here
+    input=input.trim().split("\n");
+    var N = +(input[0]);
+    var str=input[1].trim();
+    checkVowel(N,str);
   }
-  checkVowel(4,"star");
-  
+
+  function checkVowel(N,str)
+  {
+      var vowel="aeiou";
+      var M=vowel.length;
+      var isVowel=false;
+      for(var i=0;i<M;i++)
+      {
+          for(var j=0;j<N;j++)
+          {
+              if(vowel[i]===str[j])
+              {
+                  isVowel=true;
+                  break;
+              }
+          }
+      }
+      if(isVowel)
+      {
+        console.log("true");
+      }
+      else
+      {
+        console.log("false");
+      }
+  }
+  if (process.env.USERNAME === "offic") {
+    runProgram(`1
+    o`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }

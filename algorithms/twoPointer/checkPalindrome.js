661779f8729fc2accde9979d653de9b550006f5f
Siddharth Bisht <80667996+OfficialSiddharthBisht@users.noreply.github.com>
2021-11-05 15:20:46 +0530

    palindrome by 2 pointer

---
+++ algorithms/twoPointer/checkPalindrome.js
@@ -0,0 +1,31 @@
+/*  ou are given a string, whose size is stored in a variable with the name N
+The string is stored in a variable with the namestr
+PrintYes, if the string is a palindrome, else printNo
+For example, consider the value stored instr = "naman", if we reverse the string, the reversed string will be the same as the original string, hence the string is considered as palindrome, therefore, the output isYes
+Note : A palindrome is a string which is the same read forwards or backwards
+*/
+function checkPalindrome(N, str) {
+    //write code here
+    let i=0;
+    let j=N-1;
+    let isPalindrome=true;
+    while(i<j)
+    {
+        if(str[i]!==str[j])
+        {
+            isPalindrome=false;
+            break;
+        }
+        i++;
+        j--;
+    }
+  if(isPalindrome)
+  {
+      console.log("Yes");
+  }
+  else
+  {
+      console.log("No");
+  }
+}
+checkPalindrome(5,"racsr")

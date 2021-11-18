76ee06c7774ad83ab77689264edde480f7a0b6b0
Siddharth Bisht <80667996+OfficialSiddharthBisht@users.noreply.github.com>
2021-11-18 08:25:36 +0530

    Update generateArrayWithTheirFirstChar.js

--- HigherOrderFunctionsJavaScript(Advance)/Array Inbuilt Higher Order Functions/generateArrayWithTheirFirstChar.js
+++ HigherOrderFunctionsJavaScript(Advance)/Array Inbuilt Higher Order Functions/generateArrayWithTheirFirstChar.js
@@ -1,6 +1,6 @@
 /* Given an array of strings generate an array with their first characters
-Sample Input - ["Masai","School"]
-Sample Output - ["M","S"]
+Sample Input - ["Siddharth","Bisht"]
+Sample Output - ["S","B"]
 */
 let arr=["Siddharth","Bisht"];
 let newArr=arr.map(function(element){

34a1e6df621e94b0090ede98bb5f6027a11a305f
SiddharthBisht <officialsiddharthbisht@gmail.com>
2021-11-27 12:57:13 +0530

    stack

--- Queue/queueUsingArr.js
+++ Queue/queueUsingArr.js
@@ -1,50 +1,50 @@
-class Queue{
-    constructor(size){
-        this.arr= new Array(size);
-        this.maxSize=size;
-        this.front=0;
-        this.rear=0;
+class Queue {
+    constructor(size) {
+        this.arr = new Array(size);
+        this.maxSize = size;
+        this.front = 0;
+        this.rear = 0;
     }
     //methods
     // enqueue - adding an element to the queue
-    enqueue(element){
+    enqueue(element) {
         // condition of overflow
-        if(this.rear===this.maxSize){
+        if (this.rear === this.maxSize) {
             return null;
         }
         // element in a queue is added from the rear i.e back
-        this.arr[this.rear]=element;
+        this.arr[this.rear] = element;
         this.rear++;
     }
 
     //dequeue
-    dequeue(){
+    dequeue() {
         // condition for underflow
-        if(this.front===this.rear){
+        if (this.front === this.rear) {
             return null;
         }
-        let dequeuedElement= this.arr[this.front];
+        let dequeuedElement = this.arr[this.front];
         // element is deleted from the front
         delete this.arr[this.front]; //optional to delete
         this.front++;
         // after removing the element if the queue is empty then
-        if(this.front===this.rear){
-            this.front=0;
-            this.rear=0;
+        if (this.front === this.rear) {
+            this.front = 0;
+            this.rear = 0;
         }
         return dequeuedElement;
     }
 
-    getFront(){
+    getFront() {
         //condition for underflow
-        if(this.front===this.rear){
+        if (this.front === this.rear) {
             return null;
         }
         return this.arr[this.front];
     }
 }
 
-let queue= new Queue(2);
+let queue = new Queue(2);
 queue.enqueue(5);
 queue.enqueue(6);
 queue.enqueue(7);

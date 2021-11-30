ce94d194b4cbc7545f45072978e9c8fc7335fb59
SiddharthBisht <officialsiddharthbisht@gmail.com>
2021-11-30 20:41:36 +0530

    solved hackerRank question reverse array

---
+++ algorithms/twoPointer/hackerRankReverseArr.js
@@ -0,0 +1,85 @@
+/*
+! Source HackerRank -  https://www.hackerrank.com/challenges/arrays-ds/problem?isFullScreen=true
+An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  or .
+
+Reverse an array of integers.
+
+Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.
+
+Example
+
+Return .
+
+Function Description
+
+Complete the function reverseArray in the editor below.
+
+reverseArray has the following parameter(s):
+
+int A[n]: the array to reverse
+Returns
+
+int[n]: the reversed array
+Input Format
+
+The first line contains an integer,N , the number of integers in .A
+The second line contains  N space-separated integers that make up A
+*/
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+     
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function reverseArray(a) {
+    // Write your code here
+    let i=0;
+    let j=a.length-1;
+    while(i<=j){
+        let temp=a[i]
+        a[i]=a[j];
+        a[j]=temp;
+        i++;
+        j--;
+    }
+    return a;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}

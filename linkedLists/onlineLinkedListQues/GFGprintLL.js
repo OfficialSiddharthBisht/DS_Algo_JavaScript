726b8b25be6464bb23ab573d2ef2c71d5db454f8
SiddharthBisht <officialsiddharthbisht@gmail.com>
2021-12-29 02:04:23 +0530

    solved GFG Ques print linked list elements

---
+++ linkedLists/onlineLinkedListQues/GFGprintLL.js
@@ -0,0 +1,38 @@
+/*
+Print Linked List Elements : -
+Problem Source : https://practice.geeksforgeeks.org/problems/print-linked-list-elements/1/?problemType=functional&difficulty[]=-1&page=1&sortBy=submissions&query=problemTypefunctionaldifficulty[]-1page1sortBysubmissions#
+Given a linked list. Print all the elements of the linked list.
+
+Example 1:
+
+Input:
+N=2
+LinkedList={1 , 2}
+Output:
+1 2
+Explanation:
+The linked list contains two 
+elements 1 and 2.The elements 
+are printed in a single line.
+Example 2:
+
+Input:
+N = 3
+Linked List = { 49, 10, 30}
+Output: 
+49 10 30
+Explanation:
+The linked list contains 3 
+elements 49, 10 and 30. The 
+elements are printed in a single 
+line.
+Your task:
+You don't need to read input or print anything. Your task is to complete the function display() which takes the head of the linked list as input and prints the linked list in a single line.
+ 
+Expected Time Complexity : O(N)
+Expected Auxiliary Space : O(1)
+ 
+Constraints :
+1 <= N <= 100
+
+*/

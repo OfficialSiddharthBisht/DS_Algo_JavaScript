efa48192c6a5a822b3198b53207a9f94be9adbdf
SiddharthBisht <officialsiddharthbisht@gmail.com>
2022-01-02 12:26:39 +0530

    minor changes

--- algorithms/searchingAlgorithms/binarySearch/optimizeTheCurve.js
+++ algorithms/searchingAlgorithms/binarySearch/optimizeTheCurve.js
@@ -1,3 +1,40 @@
+/*
+? Optimize The Curve Using Binary Search : -
+Siddharth is skilled in calculations, he can do most of the complex calculations in head but he is stuck with this quesiton and wants your help.
+
+Siddharth wants to solve the given mathematical equation which states that, he needs to find the least non-negative integer Xo, that shall make the value of given equation atleast K i.e.,
+
+A Xo^2 + B Xo + C >= K
+
+He is given A,B,C and K, which form the above equation. He has to answer T independent test cases.
+
+Output -1 if there is no possible positive value of Xo
+
+
+Input
+Input Format :
+
+Number of test cases, space seperated values of A B C K respectively
+
+Constraints :
+
+- A, B, C <= 100000
+
+- K <= 10000000000
+
+- T <= 100000
+
+
+Output
+The value of Xo
+
+Sample Input 1 
+1
+3 2 4 15
+
+Sample Output 1
+2
+*/
 function optimizeTheCurve(A, B, C, K) {
     let ans = -1;
     let low = 0;

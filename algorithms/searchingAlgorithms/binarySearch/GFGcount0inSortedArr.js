170619bae9c05aa1a6b4b2d4f48a654b1bedb295
SiddharthBisht <officialsiddharthbisht@gmail.com>
2022-01-03 14:46:21 +0530

    minor change

--- algorithms/searchingAlgorithms/binarySearch/GFGcount0inSortedArr.js
+++ algorithms/searchingAlgorithms/binarySearch/GFGcount0inSortedArr.js
@@ -1,5 +1,8 @@
 /*
 * Count The Zeros In Sorted Array
+
+https://practice.geeksforgeeks.org/problems/count-the-zeros2550/1
+
 Given an array of size N consisting of only 0's and 1's. The array is sorted in such a manner that all the 1's are placed first and then they are followed by all the 0's. Find the count of all the 0's.
 
 Example 1:

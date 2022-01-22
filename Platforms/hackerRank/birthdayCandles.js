f6b42b6c488494213c6ffa98e3be5ea646111800
SiddharthBisht <officialsiddharthbisht@gmail.com>
2022-01-22 06:01:44 +0530

    sovled hackerRank question birthday candles

---
+++ Platforms/hackerRank/birthdayCandles.js
@@ -0,0 +1,101 @@
+/* Birthday Candles
+Question Link - https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+
+You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+Example
+
+
+The maximum height candles are  units high. There are  of them, so return .
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below.
+
+birthdayCakeCandles has the following parameter(s):
+
+int candles[n]: the candle heights
+Returns
+
+int: the number of candles that are tallest
+Input Format
+
+The first line contains a single integer, , the size of .
+The second line contains  space-separated integers, where each integer  describes the height of .
+
+Constraints
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+Explanation 0
+
+Candle heights are . The tallest candles are  units, and there are  of them.
+*/
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let N = candles.length;
+    let max = 0;
+    for(let  i = 0 ; i < N; i++){
+        if(candles[i] > max){
+            max = candles[i];
+        }
+    }
+    let count = 0;
+    for(let i = 0; i < N ; i++){
+        if(candles[i] === max){
+            count++;
+        }
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const candlesCount = parseInt(readLine().trim(), 10);
+
+    const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+    const result = birthdayCakeCandles(candles);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}

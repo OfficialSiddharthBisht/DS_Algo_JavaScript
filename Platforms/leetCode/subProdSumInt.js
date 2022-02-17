a6540239b1addf119026b03722061822f56221ab
Siddharth <officialsiddharthbisht@gmail.com>
2022-02-17 23:35:51 +0530

    solved leetcode prob with long name

---
+++ Platforms/leetCode/subProdSumInt.js
@@ -0,0 +1,43 @@
+/*
+? Subtract the Product and Sum of Digits of an Integer
+Problem Link : - https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+ 
+
+Constraints:
+
+1 <= n <= 10^5
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var subtractProductAndSum = function(n) {
+    let sum = 0;
+    let product = 1;
+    while(n !== 0){
+        sum += n % 10;
+        product *= n % 10;
+        n = parseInt(n / 10);
+    }
+    return product - sum;
+};

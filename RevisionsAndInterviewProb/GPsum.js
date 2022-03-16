d1cdfff45364de21dc34b5e8a45436ab8d6a589b
Siddharth Bisht <80667996+OfficialSiddharthBisht@users.noreply.github.com>
2022-03-16 22:56:58 +0530

    solved geometric progression sum

---
+++ RevisionsAndInterviewProb/GPsum.js
@@ -0,0 +1,9 @@
+function GPsum(N , r , curr){
+    // Base Case
+    if(curr > N){
+        return 0;
+    }
+    return (1 / Math.pow(r ,curr) + GPsum(N , r, curr + 1));
+}
+
+console.log(Number(GPsum(5 , 2 ,0)).toFixed(4));

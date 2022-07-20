4d47141f880d31e35c0cdaafefca09aac0549867
Siddharth <officialsiddharthbisht@gmail.com>
2022-07-20 12:51:21 +0530

    simpler version of merge 2 linked lists

--- LinkedList/onlineLinkedListQues/merge2LL.js
+++ LinkedList/onlineLinkedListQues/merge2LL.js
@@ -80,3 +80,34 @@ var mergeTwoLists = function(l1, l2) {
     return head;
 };
 
+// Simpler Version Of The Same :-
+
+var mergeTwoLists = function(l1, l2) {
+    let head = new ListNode(0);
+    curr = head;
+    while(l1 !== null && l2 !== null){
+        if(l1.val < l2.val){
+            curr.next = l1;
+            curr = curr.next;
+            l1 = l1.next;
+        }else if(l1.val > l2.val){
+            curr.next = l2;
+            curr = curr.next;
+            l2 = l2.next;
+        }else if(l1.val === l2.val){
+            curr.next = l1;
+            l1 = l1.next;
+            curr = curr.next;
+            curr.next = l2;
+            l2 = l2.next;
+            curr = curr.next;
+        }
+    }
+    if(l1 !== null){
+        curr.next = l1;
+    }
+    if(l2 !== null){
+        curr.next = l2;
+    }
+    return head.next;
+};

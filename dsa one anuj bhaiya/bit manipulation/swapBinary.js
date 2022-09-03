6911f30b2c65736fab5a6826c7cc940a1323c464
Siddharth <officialsiddharthbisht@gmail.com>
2022-09-03 09:29:05 +0530

    UPDATE: logic for swapping using bit manipulation

--- dsa one anuj bhaiya/bit manipulation/swapBinary.js
+++ dsa one anuj bhaiya/bit manipulation/swapBinary.js
@@ -11,3 +11,27 @@ function swap(a , b){
 }
 
 console.log(swap(5,7));
+
+/*
+5 ->  101
+7 ->  111
+a = a ^ b;
+    101
+   ^111
+    ----
+    010 = 2 therefore a = 2
+    xor => if both values are different then 1 else 0
+
+    b = a ^ b
+    010
+   ^111
+   -----
+    101 = 5 therefore b = 5
+    
+    a = a ^ b
+    010
+   ^101
+    ----
+    111 = 7 therefore a = 7
+    hence a = 7 and b = 5 swap done
+*/

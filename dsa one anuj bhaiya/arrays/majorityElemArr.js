eb0613cd16313ae60891fe52ad2d5de3b6c7140c
Siddharth <officialsiddharthbisht@gmail.com>
2022-12-17 09:12:29 +0530

    SOLVED: majority elem by hashmap

--- dsa one anuj bhaiya/arrays/majorityElemArr.js
+++ dsa one anuj bhaiya/arrays/majorityElemArr.js
@@ -27,14 +27,16 @@ function majorityElemBF(arr, N) {
     }
     return -1;
 }
-console.log(majorityElemBF([5, 1, 4, 5, 5], 5));
+// console.log(majorityElemBF([5, 1, 4, 5, 5], 5));
 
+// Using sorting technique O(log N) tc , O(1) sc
 function majorityElemSorting(arr, N) {
     arr = arr.sort((a, b) => {
         return a - b;
     });
     let maxCount = 0;
     let majorityElem = -1;
+    let count = 0;
     for (let i = 0; i < N - 1; i++) {
         if (arr[i] === arr[i + 1]) {
             count++;
@@ -49,3 +51,28 @@ function majorityElemSorting(arr, N) {
     }
     return -1;
 }
+
+// * Using key value pairs / hashmaps tc - O(N) , sc - O(N)
+function majorityElemKVP(arr, N) {
+    let kvp = {};
+    for (let i = 0; i < N; i++) {
+        if (!kvp[arr[i]]) {
+            kvp[arr[i]] = 1;
+        } else {
+            kvp[arr[i]] += 1;
+        }
+    }
+    let max = 0;
+    let elem = -1;
+    for (let key in kvp) {
+        if (kvp[key] > max) {
+            max = kvp[key]
+            elem = key;
+        }
+    }
+    if (max > parseInt(N / 2)) {
+        return elem;
+    }
+    return -1;
+}
+console.log(majorityElemSorting([5, 1, 4, 5, 5], 5));

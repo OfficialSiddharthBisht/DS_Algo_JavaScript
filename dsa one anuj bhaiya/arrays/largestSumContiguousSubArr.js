8b157fbe0c63aaaa7d43879232953b2a8406e23c
Siddharth <officialsiddharthbisht@gmail.com>
2022-12-20 17:03:23 +0530

    SOLVED: largest sum contigous subarr by Kadane's Algorithm

--- dsa one anuj bhaiya/arrays/largestSumContiguousSubArr.js
+++ dsa one anuj bhaiya/arrays/largestSumContiguousSubArr.js
@@ -3,7 +3,7 @@ Largest Sum Contiguous Subarray
 Given an array of size N , find the sum of largest subarray
 arr = [-5 , 4 , 6 ,-3 , 4 ,-1]
 */
-
+// Bruteforce TC -> O(N*N) , SC -> O(1)
 function largestSumContiguousSubArr(N, arr) {
     let maxSum = Number.MIN_SAFE_INTEGER;
     for (let i = 0; i < N; i++) {
@@ -19,3 +19,32 @@ function largestSumContiguousSubArr(N, arr) {
 }
 
 console.log(largestSumContiguousSubArr(6, [-5, 4, 6, -3, 4, -1]))
+
+//2 -> We can't apply any sorting approach for the same cauz it will disturb the order and in subarray problems order
+// matters the most
+
+// 3 -> If we try to solve the same with hashmaps/key value pairs then too once we to go to each subarray(O(N*N))
+// thereforce no optimization on doing the same
+
+// 4 -> Kadane's Algorithm
+/*
+Loop through the whole array and find the sum, but in any case if the sum is coming -ve then discart it and again 
+start with the next element
+*/
+
+function largestSumContiguousSubArrKadanesAlgo(N, arr) {
+    let maxSum = 0;
+    let currSum = 0;
+    for (let i = 0; i < N; i++) {
+        currSum += arr[i];
+        if (currSum > maxSum) {
+            maxSum = currSum;
+        }
+        if (currSum <= 0) {
+            currSum = 0;
+        }
+    }
+    return maxSum;
+}
+
+console.log(largestSumContiguousSubArrKadanesAlgo(6, [-5, 4, 6, -3, 4, -1]))
